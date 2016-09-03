8d2f08ec1a4bf78f75464cca8c753725544f75ce
EgorPyat <pyateg@gmail.com>
2016-09-03 20:42:11 +0700

    start for task_0b

---
+++ OOP/task_0b/header.h
@@ -0,0 +1,17 @@
+#ifndef H_1
+#define H_1
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+namespace Sort {
+
+  void sort();
+
+}
+
+using namespace Sort;
+
+#endif
---
+++ OOP/task_0b/sort.cpp
@@ -0,0 +1,5 @@
+#include "header.h"
+
+void Sort::sort() {
+  cout << "Hi" << endl;
+}
