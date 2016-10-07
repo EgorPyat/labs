98c7d995c7d11382e64ba2a80b8315932bce6585
EgorPyat <pyateg@gmail.com>
2016-10-07 15:00:37 +0700

    OOP task_1

--- OOP/task_1/tests.cpp
+++ OOP/task_1/tests.cpp
@@ -1,2 +1,12 @@
 #include "header.h"
-#include <gtest/gtest.h>
+
+TEST(TritTest, CapacityTest) {
+  TritSet a(1000);
+  EXPECT_EQ(1000, a.capacity());
+}
+
+TEST(TritTest, AssigmentTest){
+  TritSet b(10);
+  b[100] = True;
+  EXPECT_EQ(True, b[100]);
+}
