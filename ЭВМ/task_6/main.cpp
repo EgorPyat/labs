f5598790f5d906728f76eaf21e078663db3fd403
EgorPyat <pyateg@gmail.com>
2016-10-28 00:40:32 +0700

    ЭВМ task_6

--- ЭВМ/task_6/main.cpp
+++ ЭВМ/task_6/main.cpp
@@ -1,40 +1,49 @@
+#include <cstdlib>
+#include <cstring>
 #include <iostream>
 #include <sensors/sensors.h>
 using namespace std;
 int main() {
-    // sensors_chip_name const * cn;
-    // int c = 0;
-    // while ((cn = sensors_get_detected_chips(0, &c)) != 0) {
-    //     std::cout << "Chip: " << cn->prefix << "/" << cn->path << std::endl;
-    //
-    //     sensors_feature const *feat;
-    //     int f = 0;
-    //
-    //     while ((feat = sensors_get_features(cn, &f)) != 0) {
-    //         std::cout << f << ": " << feat->name << std::endl;
-    //
-    //         sensors_subfeature const *subf;
-    //         int s = 0;
-    //
-    //         while ((subf = sensors_get_all_subfeatures(cn, feat, &s)) != 0) {
-    //             std::cout << f << ":" << s << ":" << subf->name
-    //                       << "/" << subf->number << " = ";
-    //             double val;
-    //             if (subf->flags & SENSORS_MODE_R) {
-    //                 int rc = sensors_get_value(cn, subf->number, &val);
-    //                 if (rc < 0) {
-    //                     std::cout << "err: " << rc;
-    //                 } else {
-    //                     std::cout << val;
-    //                 }
-    //             }
-    //             std::cout << std::endl;
-    //         }
-    //     }
-    // }
   int l = 0;
-  char buf[256];
-  cout << sensors_get_detected_chips(NULL, &l) << endl;
-  // sensors_snprintf_chip_name(buf, 256, sensors_get_detected_chips(NULL, &l));
+  int k = 0;
+  int j = 0;
+  double h;
+  char* buf = new char[256];
+  sensors_chip_name const * chip;
+  sensors_feature const * feature;
+  sensors_subfeature const * subfeature;
+  sensors_cleanup();
+  sensors_init(NULL);
+
+  for(;;){
+    if((chip = sensors_get_detected_chips(NULL, &l)) != NULL){
+      sensors_snprintf_chip_name(buf, 256, chip);
+      cout << buf << endl;
+      for(;;){
+        if((feature = sensors_get_features(chip, &k)) != NULL){
+          buf = sensors_get_label(chip, feature);
+          cout << buf << endl;
+          for(;;){
+            if((subfeature = sensors_get_all_subfeatures(chip, feature, &j)) != NULL){
+                sensors_get_value(chip, subfeature->number, &h);
+                cout << subfeature->number << " " << h << endl;
+            }
+            else {
+              j = 0;
+              break;
+            }
+          }
+        }
+        else {
+          k = 0;
+          break;
+        }
+      }
+    }
+    else break;
+  }
+
+  delete[] buf;
+
   return 0;
 }
