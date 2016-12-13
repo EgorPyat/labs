2d19d02e8fa1a4bde2eb0add8491c95b18265b8b
EgorPyat <pyateg@gmail.com>
2016-12-13 20:13:45 +0700

    OOP task_3

--- OOP/task_3/tuple_print.h
+++ OOP/task_3/tuple_print.h
@@ -1,18 +1,18 @@
 #ifndef _TUP_PR
 #define _TUP_PR
 
-template<typename T>
-std::ostream& print(std::ostream& out, const T& what) {
+template<typename Ch, typename Tr, typename T>
+auto print(std::basic_ostream<Ch,Tr>& out, const T& what) -> decltype(out << what){
   return out << what;
 }
 
-template<typename T, typename... Types>
-std::ostream& print(std::ostream& out, const T& what, const Types& ... other) {
+template<typename Ch, typename Tr, typename T, typename... Types>
+auto print(std::basic_ostream<Ch,Tr>& out, const T& what, const Types& ... other) -> decltype(out << what) {
   return print(out << what << ' ', other...);
 }
 
 template<int ...>
-struct seq { };
+struct seq {};
 
 template<int N, int... S>
 struct make_range : make_range<N-1, N-1, S...> { };
@@ -22,13 +22,13 @@ struct make_range<0, S...> {
   typedef seq<S...> result;
 };
 
-template<typename... Types, int... range>
-std::ostream& tuple_print(std::ostream& out, const std::tuple<Types...>& what, const seq<range...>) {
+template<typename Ch, typename Tr, typename... Types, int... range>
+auto tuple_print(std::basic_ostream<Ch,Tr>& out, const std::tuple<Types...>& what, const seq<range...>) -> decltype(out){
   return print(out, std::get<range>(what)...);
 }
 
-template<typename... Types>
-std::ostream& operator<<(std::ostream& out, const std::tuple<Types...>& what) {
+template<typename Ch, typename Tr, typename... Types>
+auto operator<<(std::basic_ostream<Ch,Tr>& out, const std::tuple<Types...>& what) -> decltype(out) {
   return tuple_print(out, what, typename make_range<sizeof...(Types)>::result());
 }
 
