99ea358133871340dd4e949e88a843f40c72f025
Egor <Egor@EgorPyat.(none)>
2017-02-11 07:27:11 +0000

    Solaris task_21

---
+++ Solaris/task_21/signals.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+
+int count;
+void interruption(int sig){
+        if (sig == SIGQUIT) {
+		if(count == 1) printf("   %d interruption.\n", count);
+		else printf("   %d interruptions.\n", count);
+        	exit(1);
+        }
+	printf("   Oops!\n");
+        count++;
+	signal(sig, interruption);
+}
+
+int main(){
+	signal(SIGINT, interruption);
+	signal(SIGQUIT, interruption);
+
+	for (;;) pause();
+	
+	return 0;
+}
+
+                    
