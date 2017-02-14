c0e6736cec026e5599c4a7eb591d5f597f33fa67
Egor <Egor@EgorPyat.(none)>
2017-02-15 01:31:30 +0200

    OSi task_16

--- OSi/task_16/symbol.c
+++ OSi/task_16/symbol.c
@@ -5,19 +5,22 @@
 
 int main(){
 	int fd;
+	char ch;
 	struct termios terminal;
 	struct termios save_set; 
-	char ch;
 
 	fd = open("/dev/tty", O_RDWR);
 	tcgetattr(fd, &terminal);
 	save_set = terminal;
-	terminal.c_lflag = terminal.c_lflag & ( ~ICANON );
+	terminal.c_lflag &= ~ICANON;
 	terminal.c_cc[VMIN] = 1;
 	tcsetattr(fd, TCSANOW, &terminal);
-	write(fd, "Enter: ", 8);	
+
+	write(fd, "Enter some symbol: ", 19);	
+
 	read(fd, &ch, 1);
-	write(fd, "\nYou entered: ", 15);
+	
+	write(fd, "\nYou entered: ", 14);
 	write(fd, &ch, 1);
 	write(fd, "\n", 1);
 	
