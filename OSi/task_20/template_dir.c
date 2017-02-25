8b238331288910058a9dd7ca41ba4ce9a5871cd4
EgorPyat <pyateg@gmail.com>
2017-02-25 15:52:49 +0700

    OSi task_20

--- OSi/task_20/template_dir.c
+++ OSi/task_20/template_dir.c
@@ -1,22 +1,139 @@
-//#include <sys/types.h>
-//#include <unistd.h>
-//#include <stdlib.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <dirent.h>
-//#include <sys/stat.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
 
-int main(int argc, char *argv[])
-{
-	DIR *dirp;
-	struct dirent *dp;
+int suit(char* filename, char* template){
+	int i = 0;
+	int j = 0;
+	int last = 0;
+	int symb = 0;
+	int find = 0;
+	int star = 0;
+	int ques = 0;
+	int flen = strlen(filename);
+	int tlen = strlen(template);
 
-	if ((dirp = opendir(argv[1])) == NULL) {
-		perror(argv[0]);
+	for(; i < tlen; i++){
+		switch(template[i]){
+			case '/':
+				printf("Bad template format\n");
+				return 1;
+			case '?':
+				++ques;
+				break;
+			case '*':
+				star = 1;
+				break;
+			default:
+				symb = 1;
+				for(; j < flen; j++){
+					if(filename[j] == template[i]){
+						find = 1;
+						if(star == 0 && (j - last) != ques){
+							// printf("Don't match!\n");
+							return 1;
+						}
+			 			else if(star == 1 && (j - last) < ques){
+							// printf("Don't match!\n");
+							return 1;
+						}
+						j++;
+						last = j;
+						break;
+					}
+				}
+				if(find == 0){
+					// printf("Don't match! %c\n", template[i]);
+					return 1;
+				}
+				ques = 0;
+				star = 0;
+				find = 0;
+				break;
+		}
+	}
+ 	if(symb == 0 && star == 0 && flen != ques){
+		// printf("Don't match!\n");
 		return 1;
 	}
-	while ((dp = readdir(dirp)) != NULL){
-		printf("%-14.*s\n", dp->d_reclen, dp->d_name);
+	else if(symb == 0 && star == 1 && flen < ques){
+		// printf("Don't match!\n");
+		return 1;
 	}
-	closedir(dirp);
+	else if(symb == 1 && star == 0 && ques != (flen - last)){
+		// printf("Don't match!\n");
+		return 1;
+	}
+	else if(symb == 1 && star == 1 && ques > (flen - last)){
+		// printf("Don't match!\n");
+		return 1;
+	}
+	return 0;
+}
+
+char** parse(char* template){
+	printf("Template: %s\n", template);
+	int i;
+	int size = 0;
+	int len = strlen(template);
+	char** parsed;
+	int j = 0;
+	for(i = 0; i < len; i++){
+		if(template[i] == '/'){
+			++size;
+		}
+	}
+	if(size == 0) return (char**)0;
+
+	parsed = (char**)malloc(size);
+	size = 0;
+
+	for(i = 0; i < len; i++){
+		if(template[i] == '/'){
+			parsed[size] = (char*)malloc(i - j + 1);
+			memcpy(parsed[size], template + j, i - j);
+			parsed[size][i] = '\0';
+			j = i;
+			++j;
+			++size;
+		}
+	}
+
+	return parsed;
+}
+
+int main(int argc, char *argv[]){
+	DIR *dirp;
+	struct dirent *dp;
+	int find = 0;
+	char** template;
+	if(argc == 3){
+		template = parse(argv[2]);
+		if(template == NULL) return 0;
+
+		while(1){
+			write(0, template[find], strlen(template[find]));
+			++find;
+			// getchar();
+		}
+		if ((dirp = opendir(argv[1])) == NULL) {
+			perror(argv[1]);
+			return 1;
+		}
+		while ((dp = readdir(dirp)) != NULL){
+			if(!suit(dp->d_name, argv[2])) {
+				printf("%-14.*s\n", dp->d_reclen, dp->d_name);
+				++find;
+			}
+		}
+		if(find == 0) printf("Template: %s\n", argv[2]);
+
+		closedir(dirp);
+	}
+	else printf("%s: Bad arguments\n", argv[0]);
+
 	return 0;
 }
