d87903eb7f1b1451b0c241d4928019a6037f8654
Egor <Egor@EgorPyat.(none)>
2017-03-04 03:46:13 +0600

    OSi task_6

--- OSi/task_6/find_str_wait.c
+++ OSi/task_6/find_str_wait.c
@@ -1,3 +1,4 @@
+/*
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -36,7 +37,10 @@ int main(){
 			return 0;
 		};
 		num = atoi(str);
-		if(num == 0) return 0;
+		if(num == 0) {
+			close(file);
+			return 0;
+		}
 		else if(num < 0) printf("N should be > 0!\n");
 		else if(num == 1){
 			lseek(file, 0, SEEK_SET);
@@ -54,5 +58,35 @@ int main(){
 
 	close(file);
 
+	return 0;
+}
+*/
+
+#include <stdio.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(void) {
+	fd_set rfds;
+	struct timeval tv;
+	int retval;
+	char string[3];
+	/* Watch stdin (fd 0) to see when it has input. */
+	FD_ZERO(&rfds);
+	FD_SET(0, &rfds);
+	/* Wait up to five seconds. */
+	tv.tv_sec = 5;
+	tv.tv_usec = 0;
+	retval = select(1, &rfds, NULL, NULL, &tv);
+	/* Don’t rely on the value of tv now! */
+	read(0, string, 3);
+	if (retval == -1)
+	perror("select()");
+	else if (retval)
+	printf("Data is available now.\n");
+	/* FD_ISSET(0, &rfds) will be true. */
+	else
+	printf("No data within five seconds.\n");
 	return 0;
 }
