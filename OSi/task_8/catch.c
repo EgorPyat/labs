b07638566c4070fadd4036ba394dfaeba15e457e
Egor <Egor@EgorPyat.(none)>
2017-04-02 03:45:11 +0600

    OSi task_8

--- OSi/task_8/catch.c
+++ OSi/task_8/catch.c
@@ -1,6 +1,7 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdio.h>
 
 int main(int argc, char *argv[]){
 	struct flock str;
@@ -10,13 +11,14 @@ int main(int argc, char *argv[]){
 		return 1;
 	}
 
-	str.l_type = F_WRLCK;
+	str.l_type = F_WRLCK; //F_RDLCK
 	str.l_whence = SEEK_SET;
 	str.l_start = 0;
 	str.l_len = 0;
 
 	if (fcntl(file, F_SETLK, &str) == -1) {
-		return 1;
+		printf("Locked!\n");
+		return 2;
 	}
 
 	system("vi catch.c");
