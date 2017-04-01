253760ea47d80642fec4d2275d85958b3543d5c6
Egor <Egor@EgorPyat.(none)>
2017-04-01 16:03:06 +0600

    OSi task_8

--- OSi/task_8/catch.c
+++ OSi/task_8/catch.c
@@ -2,8 +2,24 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-int main(){
+int main(int argc, char *argv[]){
+	struct flock str;
+	int file;
 
+	if ((file = open("catch.c", O_RDWR)) == -1) {
+		return 1;
+	}
+
+	str.l_type = F_WRLCK;
+	str.l_whence = SEEK_SET;
+	str.l_start = 0;
+	str.l_len = 0;
+
+	if (fcntl(file, F_SETLK, &str) == -1) {
+		return 1;
+	}
+
+	system("vi catch.c");
+		
 	return 0;	
-	
 }
