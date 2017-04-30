824da7a3d889fee64e2aa376a0c9b676cd0d2cc3
Egor Pyataev <egor@localhost.localdomain>
2017-04-30 11:04:13 +0700

    Parallels task_4

--- Parallels/task_4/inrow_poisson.c
+++ Parallels/task_4/inrow_poisson.c
@@ -1,37 +1,34 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
-#include<time.h>
-#include<sys/time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <sys/time.h>
 
-/* Количество ячеек вдоль координат x, y, z */
+/* Количество ячеек вдоль координат x, y */
 #define in 20
 #define jn 20
-#define kn 20
 
 #define a 1
 
-double Fresh(double, double, double);
-double Ro(double, double, double);
+double Fresh(double, double);
+double Ro(double, double);
 void Inic();
 
 /* Выделение памяти для 3D пространства */
-double F[in+1][jn+1][kn+1];
-
-double hx, hy, hz;
-
+double F[in+1][jn+1];
+double hx, hy;
 
 /* Функция определения точного решения */
-double Fresh(double x, double y, double z){
+double Fresh(double x, double y){
   double res;
-  res = x + y + z;
+  res = x + y;
   return res;
 }
 
 /* Функция задания правой части уравнения */
-double Ro(double x, double y, double z){
+double Ro(double x, double y){
   double d;
-  d = -a*(x+y+z);
+  d = -a*(x+y);
   return d;
 }
 
@@ -40,26 +37,23 @@ void Inic(){
   int i, j, k;
   for(i = 0; i <= in; i++){
     for(j = 0; j <= jn; j++){
-      for(k = 0; k <= kn; k++){
-        if((i!=0)&&(j!=0)&&(k!=0)&&(i!=in)&&(j!=jn)&&(k!=kn)){
-          F[i][j][k] = 0;
-        }
-        else{
-          F[i][j][k] = Fresh(i*hx,j*hy,k*hz);
-        }
+      if((i!=0)&&(j!=0)&&(i!=in)&&(j!=jn)){
+        F[i][j] = 0;
+      }
+      else{
+        F[i][j] = Fresh(i*hx,j*hy);
       }
     }
   }
 }
 
-
 int main(){
-  double X, Y, Z;
+  double X, Y;
   double max, N, t1, t2;
-  double owx, owy, owz, c, e;
-  double Fi, Fj, Fk, F1;
+  double owx, owy, c, e;
+  double Fi, Fj, F1;
 
-  int i, j, k, mi, mj, mk;
+  int i, j,mi, mj;
   int R, fl, fl1, fl2;
   int it,f;
   long int osdt;
@@ -69,65 +63,57 @@ int main(){
   it = 0;
   X = 2.0;
   Y = 2.0;
-  Z = 2.0;
   e = 0.00001;
 
   /* Размеры шагов */
   hx = X/in;
   hy = Y/jn;
-  hz = Z/kn;
 
-  owx = pow(hx,2);
-  owy = pow(hy,2);
-  owz = pow(hz,2);
+  owx = hx * hx;
+  owy = hy * hy;
 
-  c = 2/owx + 2/owy + 2/owz + a;
+  c = 2/owx + 2/owy + a;
 
   gettimeofday(&tv1,(struct timezone*)0);
 
   /* Инициализация границ пространства */
   Inic();
 
-/* Основной итерационный цикл */
+  /* Основной итерационный цикл */
   do{
     f = 1;
-    for(i = 1; i < in; i++)
+    for(i = 1; i < in; i++){
       for(j = 1; j < jn; j++){
-        for(k = 1; k < kn; k++){
-          F1 = F[i][j][k];
-          Fi = (F[i+1][j][k] + F[i-1][j][k]) / owx;
-          Fj = (F[i][j+1][k] + F[i][j-1][k]) / owy;
-          Fk = (F[i][j][k+1] + F[i][j][k-1]) / owz;
-          F[i][j][k] = (Fi + Fj + Fk - Ro(i*hx,j*hy,k*hz)) / c;
-          if(fabs(F[i][j][k] - F1) > e) f = 0;
-        }
+          F1 = F[i][j];
+          Fi = (F[i+1][j] + F[i-1][j]) / owx;
+          Fj = (F[i][j+1] + F[i][j-1]) / owy;
+          F[i][j] = (Fi + Fj - Ro(i*hx, j*hy)) / c;
+          if(fabs(F[i][j] - F1) > e) f = 0;
       }
-
-      it++;
+    }
+    it++;
   }
   while(f == 0);
 
   gettimeofday(&tv2,(struct timezone*)0);
   osdt = (tv2.tv_sec - tv1.tv_sec)*1000000 + tv2.tv_usec-tv1.tv_usec;
 
-  printf("\n in = %d|iter = %d|E = %f|T = %ld\n", in, it, e, osdt);
+  printf("\nin = %d | iter = %d | E = %f | T = %ld\n", in, it, e, osdt);
 
-/* Нахождение максимального расхождения полученного приближенного решения
- * и точного решения */
+  /* Нахождение максимального расхождения полученного приближенного решения
+   * и точного решения */
   max = 0.0;
 
-  for(i = 1; i <= in; i++){
+  for(i = 1; i < in; i++){
     for(j = 1; j < jn; j++){
-      for(k = 1; k < kn; k++){
-        if((F1 = fabs(F[i][j][k] - Fresh(i*hx,j*hy,k*hz))) > max){
-          max = F1;
-          mi = i; mj = j; mk = k;
-        }
-		  }
-    }
+      if((F1 = fabs(F[i][j] - Fresh(i*hx, j*hy))) > max){
+        max = F1;
+        mi = i; mj = j;
+      }
+	  }
   }
 
-  printf("Max differ = %f\n in point(%d, %d, %d)\n", max, mi, mj, mk);
+  printf("Max differ = %f in point: (%d, %d)\n", max, mi, mj);
 
   return 0;
 }
