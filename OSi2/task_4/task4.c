296497b8f5fbc1191e958cae8179726bb9111972
EgorPyat <pyateg@gmail.com>
2017-09-04 20:55:30 +0700

    OSi2 task_4

---
+++ OSi2/task_4/task4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+
+void* func();
+
+int main(){
+	pthread_t thr;
+	int i;
+	char *m1[] = {"A", "1", "B", "2", "C"};
+
+	if(pthread_create(&thr, NULL, func, m1) != 0){
+		return 1;
+	}
+
+  sleep(2);
+
+  pthread_cancel(thr);
+
+  return 0;
+}
+
+void* func(void* ptr){
+	char** message;
+	int i;
+	message = (char**)ptr;
+
+	for(i = 0; i < 5; i++){
+		printf("%s\n", message[i]);
+    sleep(1);
+	}
+}
