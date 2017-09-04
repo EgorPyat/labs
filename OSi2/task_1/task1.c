706643e4d80a67547a6764a05a80a2d141027dd3
Egor Pyataev <egor@localhost.localdomain>
2017-09-04 18:21:24 +0700

    OSi task_1

--- OSi2/task_1/task1.c
+++ OSi2/task_1/task1.c
@@ -6,23 +6,23 @@ void* func();
 
 int main(){
 	pthread_t thr;
-	int i;	
-	
+	int i;
+
 	if(pthread_create(&thr, NULL, func, NULL) != 0){
 		return 1;
-	} 	
+	}
 
 	for(i = 0; i < 10; i++){
 		printf("Parent's String#%d\n", i);
 		sleep(1);
 	}
-	
+
 	return 0;
 }
 
 void* func(void* ptr){
 	int i;
-	
+
 	for(i = 0; i < 10; i++){
 		printf("Child's String#%d\n", i);
 		sleep(2);
