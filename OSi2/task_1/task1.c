7817efda2588399e81710bcb388a058e48da92c8
Egor <Egor@EgorPyat.(none)>
2017-09-04 16:41:43 +0600

    OSi2 task_1/2

--- OSi2/task_1/task1.c
+++ OSi2/task_1/task1.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
+#include <pthread.h>
+
+void* func();
 
 int main(){
+	pthread_t thr;
+	int i;	
+	
+	if(pthread_create(&thr, NULL, func, NULL) != 0){
+		return 1;
+	} 	
 
-  return 0;
+	for(i = 0; i < 100; i++){
+		printf("Parent's String#%d\n", i);
+	}
+	
+	return 0;
+}
+
+void* func(void* ptr){
+	int i;
+	
+	for(i = 0; i < 10; i++){
+		printf("Child's String#%d\n", i);
+	}
 }
--- OSi2/task_2/task2.c
+++ OSi2/task_2/task2.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
+#include <pthread.h>
+
+void* func();
 
 int main(){
+	pthread_t thr;
+	int i;	
+	
+	if(pthread_create(&thr, NULL, func, NULL) != 0){
+		return 1;
+	} 	
 
-  return 0;
+	pthread_join(thr, NULL);
+
+	for(i = 0; i < 10; i++){
+		printf("Parent's String#%d\n", i);
+	}
+	
+	return 0;
+}
+
+void* func(void* ptr){
+	int i;
+	
+	for(i = 0; i < 10; i++){
+		printf("Child's String#%d\n", i);
+	}
 }
