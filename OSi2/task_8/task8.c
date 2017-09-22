3d04a23f45317095dd596571c589265646a59402
EgorPyat <pyateg@gmail.com>
2017-09-22 22:36:19 +0700

    task_8

---
+++ OSi2/task_8/task8.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+
+int threads_num = 4;
+int stopped = 0;
+
+void* calc_pi(void*);
+void sighandler(int);
+
+int main(int argc, char* argv[]){
+  double *retval;
+  double sum;
+  double pi = 0.0;
+  int* args;
+  pthread_t* threads;
+
+  signal(SIGINT, sighandler);
+
+  if(argc == 2) threads_num = atoi(argv[1]);
+
+  threads = (pthread_t*)malloc(threads_num * sizeof(pthread_t));
+  args = (int*)malloc(threads_num * sizeof(int));
+
+  for(int i = 0; i < threads_num; i++){
+    args[i] = i;
+    if(0 != pthread_create(&threads[i], NULL, calc_pi, (void*)(args + i))) printf("Err\n");
+  }
+
+  for(int i = 0; i < threads_num; i++){
+    if(0 != pthread_join(threads[i], (void**)(&retval))) printf("Err\n");
+    pi += *(double*)retval;
+    free(retval);
+  }
+
+  printf("PI: %f\n", pi);
+
+  free(args);
+  free(threads);
+
+  pthread_exit(NULL);
+}
+
+void* calc_pi(void* arg){
+  double *pi = (double*)malloc(sizeof(double));
+  *pi = 0.0;
+
+  for(int i = *(int*)arg; ; i += threads_num){
+    *pi += 1.0/(i*4.0 + 1.0);
+    *pi -= 1.0/(i*4.0 + 3.0);
+  }
+
+  *pi = *pi * 4.0;
+
+  printf("THR_N: %d | RES: %f\n", *(int*)arg, *pi);
+
+  pthread_exit((void*)pi);
+}
+
+void sighandler(int signum){
+  if(stopped == 0) stopped = 1;
+  else exit(0);
+}
