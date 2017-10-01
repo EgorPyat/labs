69c81a8f2df9b2fc67dec706d07b1d12eaf4960f
EgorPyat <pyateg@gmail.com>
2017-10-01 22:26:33 +0700

    Nets task_2

--- Nets/task_2/downloads/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main{
-  public static void main(String[] args) throws Exception {
-    if(args.length == 2){
-      CopyCounter c = new CopyCounter(args[0], new Integer(args[1]));
-      c.startCount();
-    }
-  }
-}
