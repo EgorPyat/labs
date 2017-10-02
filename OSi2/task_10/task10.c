4446733b10ca585e1f7ff5a1f2a569ae14f7ae28
EgorPyat <pyateg@gmail.com>
2017-10-02 19:45:21 +0700

    OSi2 task_10

---
+++ OSi2/task_10/task10.c
@@ -0,0 +1,67 @@
+#include <unistd.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+pthread_mutex_t mute[3];
+
+int flag = 0;
+
+void* print_message(void* str){
+  int k = 1;
+  int err;
+
+  pthread_mutex_lock(&mute[2]);
+
+  if(flag){
+    pthread_mutex_unlock(&mute[0]);
+  }
+
+  for(int i = 0; i < 10 * 3; i++){
+    if(pthread_mutex_lock(&mute[k]) != 0){
+      printf("Err\n");
+    }
+    k = (k + 1) % 3;
+    if(pthread_mutex_unlock(&mute[k]) != 0){
+      printf("Err\n");
+    }
+    if(k == 2){
+      printf("%s's String\n", (char*)str);
+      flag = 1;
+    }
+    k = (k + 1) % 3;
+  }
+
+  pthread_mutex_unlock(&mute[2]);
+}
+
+int main(){
+  pthread_t pthread;
+  pthread_mutexattr_t mattr;
+
+  pthread_mutexattr_init(&mattr);
+  pthread_mutexattr_settype(&mattr, PTHREAD_MUTEX_ERRORCHECK);
+
+  for(int i = 0; i < 3; i++){
+    pthread_mutex_init(&mute[i], &mattr);
+  }
+
+  pthread_mutex_lock(&mute[0]);
+
+  pthread_create(&pthread, NULL, print_message, (void*)"Child");
+
+  while(!flag){
+    sleep(1);
+  }
+
+  print_message((void*)"Parent");
+
+  pthread_join(pthread, NULL);
+
+  for(int i = 0; i < 3; i++){
+    pthread_mutex_destroy(&mute[i]);
+  }
+
+  return 0;
+}
