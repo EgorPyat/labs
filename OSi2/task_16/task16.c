34d1354c6729b437cc967912edd25a96b27a417e
Егор Пятаев <e.piataev@g.nsu.ru>
2017-11-05 19:35:55 +0700

    OSi2 task_16

--- OSi2/task_16/task16.c
+++ OSi2/task_16/task16.c
@@ -17,25 +17,33 @@ List* create_list(List* head){
   head->next = NULL;
 }
 
-void new_node(List** head, char* str){
-	List *t = NULL;
-	if(*head == NULL){
+void new_node(List* head, char* str){
+	if(head == NULL){
+		printf("%s\n", "Head isnt exist!");
 		return;
 	}
-  else if((*head)->string == NULL){
-    (*head)->string = (char*)malloc(strlen(str) + 1);
-    strcpy((*head)->string, str);
+  else if((head)->string == NULL){
+		pthread_mutex_lock(&synchronzer);
+    (head)->string = (char*)malloc(strlen(str) + 1);
+    strcpy((head)->string, str);
+		pthread_mutex_unlock(&synchronzer);
   }
   else{
-    t = (List*)malloc(sizeof(List));
-    t->string = (char*)malloc(strlen(str) + 1);
-    strcpy(t->string, str);
-    t->next = *head;
-    *head = t;
+		pthread_mutex_lock(&synchronzer);
+		List* tmp = head;
+		while(tmp->next != NULL){
+			tmp = tmp->next;
+		}
+		tmp->next = (List*)malloc(sizeof(List));
+    tmp->next->string = (char*)malloc(strlen(str) + 1);
+    strcpy(tmp->next->string, str);
+    tmp->next->next = NULL;
+		pthread_mutex_unlock(&synchronzer);
   }
 }
 
 void print_list(List *head){
+	pthread_mutex_lock(&synchronzer);
   printf("\nList:\n");
 	List *t = NULL;
 	while(head != NULL){
@@ -43,11 +51,37 @@ void print_list(List *head){
 		t = head->next;
 		head = t;
 	}
+	pthread_mutex_unlock(&synchronzer);
+}
+
+void swap(List* a, List* b){
+	char* c = a->string;
+	a->string = b->string;
+	b->string = c;
+}
+
+void* sort_list(void* head){
+	for(;;){
+		sleep(5);
+		if(((List*)head)->string == NULL){ continue; }
+		pthread_mutex_lock(&synchronzer);
+		for(List* i = (List*)head; i != NULL; i = i->next){
+			for(List* j = i->next; j != NULL; j = j->next){
+				if(strcmp(i->string, j->string) > 0){
+					swap(i, j);
+				}
+			}
+		}
+		pthread_mutex_unlock(&synchronzer);
+	}
 }
 
 int main(){
   char tmp[64];
 	List *head = create_list(head);
+	pthread_t sorter;
+	pthread_mutex_init(&synchronzer, NULL);
+	pthread_create(&sorter, NULL, sort_list, (void*)head);
 
 	for(;;){
 		fgets(tmp, 64, stdin);
@@ -56,21 +90,9 @@ int main(){
       print_list(head);
 		}
 		else{
-      new_node(&head, tmp);
+      new_node(head, tmp);
     }
   }
 
 	return 0;
 }
-
-// List* reverse(List *head){
-//   List *p = NULL;
-//   List *t = NULL;
-//   while(head != NULL){
-//     p = head;
-//     head = head->next;
-//     p->next = t;
-//     t = p;
-//   }
-//   return t;
-// }
