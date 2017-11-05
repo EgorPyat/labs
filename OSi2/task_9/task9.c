b75316f67129ee6e920c0b1113578fafd104b059
Егор Пятаев <e.piataev@g.nsu.ru>
2017-11-05 21:28:50 +0700

    OSi2 task_9

--- OSi2/task_9/task9.c
+++ OSi2/task_9/task9.c
@@ -8,6 +8,7 @@
 #define FOOD 50
 
 pthread_mutex_t forks[PHILO];
+pthread_mutex_t lforks;
 pthread_t phils[PHILO];
 void *philosopher (void *id);
 int food_on_table ();
@@ -27,6 +28,7 @@ main (int argn,
     sleep_seconds = atoi (argv[1]);
 
   pthread_mutex_init (&foodlock, NULL);
+  pthread_mutex_init (&lforks, NULL);
   for (i = 0; i < PHILO; i++)
     pthread_mutex_init (&forks[i], NULL);
   for (i = 0; i < PHILO; i++)
@@ -44,30 +46,15 @@ philosopher (void *num)
 
   id = (int)num;
   printf ("Philosopher %d sitting down to dinner.\n", id);
-  right_fork = id;
-  left_fork = id + 1;
+  right_fork = id % PHILO;
+  left_fork = (id + 1) % PHILO;
 
-  /* Wrap around the forks. */
-  // if (left_fork == PHILO)
-  //   left_fork = 0;
-  if (left_fork == PHILO) {
-     left_fork = right_fork;
-     right_fork=0;
-   }
   while (f = food_on_table ()) {
-
-    /* Thanks to philosophers #1 who would like to
-     * take a nap before picking up the forks, the other
-     * philosophers may be able to eat their dishes and
-     * not deadlock.
-     */
-    // if (id == 1)
-    //   sleep (sleep_seconds);
-
     printf ("Philosopher %d: get dish %d.\n", id, f);
-    get_fork (id, right_fork, "right");
+    pthread_mutex_lock(&lforks);
     get_fork (id, left_fork, "left ");
-
+    get_fork (id, right_fork, "right");
+    pthread_mutex_unlock(&lforks);
     printf ("Philosopher %d: eating.\n", id);
     usleep (DELAY * (FOOD - f + 1));
     down_forks (left_fork, right_fork);
