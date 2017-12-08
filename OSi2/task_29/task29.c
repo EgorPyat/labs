bbbde5db09c0147a915a1a11040a78b9dd5c6528
Егор Пятаев <e.piataev@g.nsu.ru>
2017-12-08 16:29:36 +0700

    OSi2 task_29

--- OSi2/task_29/task29.c
+++ OSi2/task_29/task29.c
@@ -19,8 +19,9 @@
 #define REQUESTSNUM 10
 
 typedef struct{
-  char* name;
+  int fd;
   char* buffer;
+  int size;
 } request;
 
 typedef struct{
@@ -28,22 +29,12 @@ typedef struct{
   char* content;
 } proxy_entry;
 
-int main(int argc, char *argv[]){
-  int    len, rc, on = 1;
-  int    cache_entry_num = 0;
-  int    listen_sd = -1, new_sd = -1;
-  int    desc_ready, end_server = FALSE, compress_array = FALSE;
-  int    close_conn;
-  char   buffer[4096];
+int create_server(){
+  int on = 1;
+  int rc;
   struct sockaddr_in   addr;
-  int    timeout;
-  struct pollfd fds[200];
-  int    nfds = 1, current_size = 0, i, j;
 
-  proxy_entry* cache = (proxy_entry*)malloc(sizeof(proxy_entry) * ENTRIESNUM);
-  request* requests = (request*)malloc(sizeof(request) * REQUESTSNUM);
-
-  listen_sd = socket(AF_INET, SOCK_STREAM, 0);
+  int listen_sd = socket(AF_INET, SOCK_STREAM, 0);
   if(listen_sd < 0){
     perror("socket() failed");
     exit(-1);
@@ -80,6 +71,55 @@ int main(int argc, char *argv[]){
     close(listen_sd);
     exit(-1);
   }
+  return listen_sd;
+}
+
+void accept_connections(int* listen_sd, struct pollfd * fds, int* nfds, int* end_server){
+  int new_sd = -1;
+  int rc;
+  int on = 1;
+
+  do{
+    new_sd = accept(*listen_sd, NULL, NULL);
+    if(new_sd < 0){
+      if(errno != EWOULDBLOCK){
+        perror("  accept() failed");
+        *end_server = TRUE;
+      }
+      break;
+    }
+    rc = ioctl(new_sd, FIONBIO, (char*)&on);
+    if(rc < 0){
+      perror("  ioctl() failed");
+      close(new_sd);
+      break;
+    }
+    else{
+      printf("  New incoming connection - %d\n", new_sd);
+      fds[*nfds].fd = new_sd;
+      fds[*nfds].events = POLLIN;
+      (*nfds)++;
+    }
+  }
+  while(new_sd != -1);
+}
+
+int main(int argc, char *argv[]){
+  int    len, rc, on = 1;
+  int    cache_entry_num = 0;
+  int    listen_sd = -1, new_sd = -1;
+  int    desc_ready, end_server = FALSE, compress_array = FALSE;
+  int    close_conn;
+  char   buffer[16384];
+  struct sockaddr_in   addr;
+  int    timeout;
+  struct pollfd fds[200];
+  int    nfds = 1, current_size = 0, i, j;
+
+  proxy_entry* cache = (proxy_entry*)malloc(sizeof(proxy_entry) * ENTRIESNUM);
+  request* requests = (request*)malloc(sizeof(request) * REQUESTSNUM);
+
+  listen_sd = create_server();
 
   memset(fds, 0, sizeof(fds));
 
@@ -96,8 +136,7 @@ int main(int argc, char *argv[]){
       perror("  poll() failed");
       break;
     }
-
-    if(rc == 0){
+    else if(rc == 0){
       printf("  poll() timed out.  End program.\n");
       break;
     }
@@ -111,31 +150,9 @@ int main(int argc, char *argv[]){
       if(fds[i].fd == listen_sd){
         printf("  Listening socket is readable\n");
 
-        do{
-          new_sd = accept(listen_sd, NULL, NULL);
-          if(new_sd < 0){
-            if(errno != EWOULDBLOCK){
-              perror("  accept() failed");
-              end_server = TRUE;
-            }
-            errno = 0;
-            break;
-          }
-          rc = ioctl(new_sd, FIONBIO, (char *)&on);
-          if(rc < 0){
-            perror("  ioctl() failed");
-            close(new_sd);
-            // exit(-1);
-          }
-          else{
-            printf("  New incoming connection - %d\n", new_sd);
-            fds[nfds].fd = new_sd;
-            fds[nfds].events = POLLIN;
-            nfds++;
-          }
-        }
-        while(new_sd != -1);
+        accept_connections(&listen_sd, fds, &nfds, &end_server);
       }
+/*_________________________POLLIN_________________________*/
       else if(fds[i].revents == POLLIN){
         printf("  Descriptor %d is readable\n", fds[i].fd);
         close_conn = FALSE;
@@ -162,69 +179,116 @@ int main(int argc, char *argv[]){
 
                 char *method = strtok(request_head, " ");
 
-                if(strcmp(method, "HEAD") == 0){
-                  printf("Method not implemented!\n");
+                if(strcmp(method, "GET") == 0){
+                  // printf("Method not implemented!\n");
+                  // close_conn = TRUE;
+                  // free(request_head);
+                  // break;
+                  char *url = strtok(NULL, " ");
+                  char *version = strtok(NULL, "\n\0");
+                  char *name = strstr(url, "://");
+                  char *name_end = strchr(name + 3, '/');
+
+                  length = 0;
+
+                  if (name_end == NULL){
+                    length = strlen(name + 3);
+                  }
+                  else{
+                    length = name_end - (name + 3);
+                  }
+
+                  char * host_name = (char*)malloc(length + 1);
+                  strncpy(host_name, name + 3, length);
+                  host_name[length] = '\0';
+                  printf("%lu\n", strlen(host_name));
+                  printf("\nMETHOD: %s\n", method);
+                  printf("URL: %s\n", url);
+                  printf("VERSION: %s\n", version);
+                  printf("HOSTNAME: %s\n\n", host_name);
+
+                  struct hostent * host_info = gethostbyname(host_name);
+
+                  free(request_head);
+                  free(host_name);
+
+                  if(host_info == NULL){
+                    printf("  gethostbyname() failed\n");
+                    break;
+                  }
+
+                  struct sockaddr_in destinationAddress;
+
+                  destinationAddress.sin_family = AF_INET;
+                  destinationAddress.sin_port = htons(80);
+                  memcpy(&destinationAddress.sin_addr, host_info->h_addr, host_info->h_length);
+
+                  int destnation = socket(AF_INET, SOCK_STREAM, 0);
+                  if(destnation < 0){
+                    perror(" socket() failed");
+                    close_conn = TRUE;
+                    break;
+                  }
+
+                  rc = ioctl(destnation, FIONBIO, (char *)&on);
+                  if(rc < 0){
+                    perror("ioctl() failed");
+                    close(destnation);
+                    close_conn = TRUE;
+                    break;
+                  }
+
+                  int con = connect(destnation, (struct sockaddr *)&destinationAddress, sizeof(destinationAddress));
+
+                  if(con < 0){
+                    if(errno != EINPROGRESS){
+                      perror("  connect() failed");
+                      close(destnation);
+                      close_conn = TRUE;
+                      break;
+                    }
+                    else{
+                      fds[nfds].fd = destnation;
+                      fds[nfds].events = POLLOUT | POLLIN;
+                      requests[nfds].fd = i;
+                      requests[nfds].buffer = (char*)malloc(sum);
+                      requests[nfds].size = sum;
+                      memcpy(requests[nfds].buffer, buffer, sum);
+                      nfds++;
+                      printf("Connection in progress\n");
+                    }
+                  }
+                  else{
+                    fds[nfds].fd = destnation;
+                    fds[nfds].events = POLLOUT;
+                    requests[nfds].fd = i;
+                    requests[nfds].buffer = (char*)malloc(sum);
+                    requests[nfds].size = sum;
+                    memcpy(requests[nfds].buffer, buffer, sum);
+                    nfds++;
+                    printf("connected to destnation point\n");
+                  }
+                }
+                else if(strstr(method, "HTTP") != NULL){
+                  printf("get response\n");
+                  requests[requests[i].fd].fd = -1;
+                  requests[requests[i].fd].buffer = (char*)malloc(sum);
+                  requests[requests[i].fd].size = sum;
+                  memcpy(requests[requests[i].fd].buffer, buffer, sum);
+                  fds[requests[i].fd].events = POLLOUT;
+                  requests[i].fd = -1;
+                  free(requests[i].buffer);
+                  requests[i].size = -1;
                   close_conn = TRUE;
                   break;
                 }
-
-								char *url = strtok(NULL, " ");
-								char *version = strtok(NULL, "\n\0");
-                char *name = strstr(url, "://");
-                char *name_end = strchr(name + 3, '/');
-
-								length = 0;
-
-								if (name_end == NULL){
-									length = strlen(name + 3);
-								}
-								else{
-									length = name_end - (name + 3);
-								}
-
-								char * host_name = (char*)malloc(length + 1);
-								strncpy(host_name, name + 3, length);
-								host_name[length] = '\0';
-                printf("%lu\n", strlen(host_name));
-                printf("\nMETHOD: %s\n", method);
-								printf("URL: %s\n", url);
-								printf("VERSION: %s\n", version);
-                printf("HOSTNAME: %s\n\n", host_name);
-
-                struct hostent * host_info = gethostbyname(host_name);
-
-                if(host_info == NULL){
-									printf("   gethostbyname() failed\n");
+                else{
+                  printf("Method not implemented!\n");
+                  close_conn = TRUE;
+                  free(request_head);
                   break;
-								}
+                }
 
-								struct sockaddr_in destinationAddress;
-
-								destinationAddress.sin_family = AF_INET;
-								destinationAddress.sin_port = htons(80);
-								memcpy(&destinationAddress.sin_addr, host_info->h_addr, host_info->h_length);
-
-								int destnation = socket(AF_INET, SOCK_STREAM, 0);
-                if(destnation < 0){
-									perror(" socket() failed");
-                  break;
-								}
-
-								if(connect(destnation, (struct sockaddr *)&destinationAddress, sizeof(destinationAddress)) < 0){
-									perror("  connect() failed");
-                  break;
-								}
-
-                fds[nfds].fd = destnation;
-                fds[nfds].events = POLLOUT;
-                // requests[nfds].socket = fds[nfds].fd;
-                requests[nfds].buffer = (char*)malloc(sum);
-                memcpy(requests[nfds].buffer, buffer, sum);
-                nfds++;
-                printf("connected to destnation point\n");
-
-                free(request_head);
-                free(host_name);
               }
             }
             break;
@@ -246,16 +310,35 @@ int main(int argc, char *argv[]){
           compress_array = TRUE;
         }
       }
+      /*________________________________________________________*/
       else if(fds[i].revents == POLLOUT){
-        // send(fds[i].fd, requests[i].buffer, sum);
-
+        printf("gonna send\n");
+        rc = send(fds[i].fd, requests[i].buffer, requests[i].size, 0);
+        if(rc < 0){
+          perror("  send() failed");
+          close_conn = TRUE;
+        }
+        else{
+          if(requests[i].fd == -1){
+            close_conn = TRUE;
+          }
+          else{
+            fds[i].events = POLLIN;
+          }
+        }
+        if(close_conn){
+          close(fds[i].fd);
+          fds[i].fd == -1;
+          compress_array = TRUE;
+        }
+        getchar();
       }
     }
 
     if(compress_array){
       compress_array = FALSE;
       for(i = 0; i < nfds; i++){
-        if (fds[i].fd == -1){
+        if(fds[i].fd == -1){
           for(j = i; j < nfds; j++){
             fds[j].fd = fds[j+1].fd;
           }
