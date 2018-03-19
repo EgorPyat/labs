47b4dd0f91bc4fa546e5c338786eb293cbe355f8
Егор Пятаев <e.piataev@g.nsu.ru>
2018-03-20 01:20:26 +0700

    ComputerGraphics task_2

--- ComputerGraphics/task_2/GUI.java
+++ ComputerGraphics/task_2/GUI.java
@@ -10,7 +10,7 @@ import javax.swing.event.*;
 
 public class GUI extends JFrame{
   public GUI(){
-    super("Photojop.");
+    super("Filters.");
 
     JMenuBar menu = new JMenuBar();
     JMenu mFile = new JMenu("File");
@@ -25,7 +25,7 @@ public class GUI extends JFrame{
     JScrollPane scrollpane = new JScrollPane(new AreasPanel());
     setLayout(new BorderLayout());
     setPreferredSize(new Dimension(800, 600));
-    add(scrollpane, BorderLayout .CENTER);
+    add(scrollpane, BorderLayout.CENTER);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     pack();
     setLocationRelativeTo(null);
@@ -35,21 +35,24 @@ public class GUI extends JFrame{
 
 class AreasPanel extends JPanel{
   private BufferedImage image;
+  private BufferedImage subimage;
+  private int X = 0;
+  private int Y = 0;
   private double scale;
   private Rectangle imageBounds;
   private Rectangle selector;
   private boolean dragging;
+  private boolean visible;
 
   public AreasPanel(){
     setPreferredSize(new Dimension(1110, 400));
 
     try{
-      image = ImageIO.read(getClass().getResource("p.jpeg"));
+      image = ImageIO.read(getClass().getResource("m.jpg"));
     }
     catch(IOException ex) {
       System.err.println(ex.getMessage());
     }
-
 
     int imgH = image.getHeight();
     int imgW = image.getWidth();
@@ -57,44 +60,106 @@ class AreasPanel extends JPanel{
     double scaleH = 350 / (double)imgH;
     double scaleW = 350 / (double)imgW;
     scale = scaleH < scaleW ? scaleH : scaleW;
-
-    selector = new Rectangle(0, 0, (int)(350 * scale), (int)(350 * scale));
+    if(scale >= 1.0){
+      scale = 1.0;
+    }
+    if(scale == 1.0){
+      // System.out.println("1.0");
+      selector = new Rectangle(0, 0, imgW, imgH);
+    }
+    else{
+      selector = new Rectangle(0, 0, (int)(350 * scale), (int)(350 * scale));
+    }
+    // System.out.println(scale);
     int w = (int)(imgW * scale);
     int h = (int)(imgH * scale);
     imageBounds = new Rectangle(15, 15, w, h);
-    int x = (w) / 2 + 15;
-    int y = (h) / 2 + 15;
-    System.out.println((w) + " " + (h ));
-    selector.setLocation(x - selector.width / 2, y - selector.height / 2);
 
-
-    Point offset = new Point();
     addMouseListener(new MouseAdapter(){
       @Override
       public void mousePressed(MouseEvent e){
-        Point p = e.getPoint();
-        if(selector.contains(p)){
-          offset.x = p.x - selector.x;
-          offset.y = p.y - selector.y;
-          dragging = true;
+        visible = true;
+        dragging = true;
+        int x = e.getX() - selector.width / 2;
+        int y = e.getY() - selector.height / 2;
+        setSelector(x, y);
+        X = (int)((selector.x - 15) / scale);
+        Y = (int)((selector.y - 15) / scale);
+        System.out.println(X + " " + Y);
+        int shiftX = 0;
+        int shiftY = 0;
+        int sideX = 350;
+        int sideY = 350;
+        if(scale == 1.0){
+          sideX = image.getWidth();
+          sideY = image.getHeight();
         }
+        if(Y + sideY > image.getHeight()){
+          shiftY = image.getHeight() - Y - sideY;
+          System.out.println("shiftY " + shiftY);
+        }
+        if(X + sideX > image.getWidth()){
+          System.out.println("shiftX " + shiftX);
+          shiftX = image.getWidth() - X - sideX;
+        }
+        try{
+          // System.out.println(X + " " + Y);
+          subimage = image.getSubimage(X + shiftX, Y + shiftY, sideX, sideY);
+        }
+        catch(Exception ex){
+          System.out.println(ex.getMessage());
+        }
+        repaint();
       }
 
       @Override
       public void mouseReleased(MouseEvent e){
         dragging = false;
+        visible = false;
+        repaint();
       }
     });
     addMouseMotionListener(new MouseMotionAdapter(){
       @Override
       public void mouseDragged(MouseEvent e){
         if(dragging){
-          int x = e.getX() - offset.x;
-          int y = e.getY() - offset.y;
-          setSelector(x, y);
+          int x = e.getX();
+          int y = e.getY();
+          setSelector(x - selector.width / 2, y - selector.height / 2);
+          X = (int)((selector.x - 15) / scale);
+          Y = (int)((selector.y - 15) / scale);
+          System.out.println(X + " " + Y);
+
+          // System.out.println((int)((selector.y) / scale));
+          int shiftX = 0;
+          int shiftY = 0;
+          int sideX = 350;
+          int sideY = 350;
+          if(scale == 1.0){
+            sideX = image.getWidth();
+            sideY = image.getHeight();
+          }
+          if(Y + sideY > image.getHeight()){
+            shiftY = image.getHeight() - Y - sideY;
+            System.out.println("shiftY " + shiftY);
+          }
+          if(X + sideX > image.getWidth()){
+            System.out.println(image.getWidth());
+            shiftX = image.getWidth() - X - sideX;
+            System.out.println("shiftX " + shiftX);
+          }
+          try{
+            // System.out.println(X + " " + Y);
+            subimage = image.getSubimage(X + shiftX, Y + shiftY, sideX, sideY);
+          }
+          catch(Exception ex){
+            System.out.println(ex.getMessage());
+          }
+          repaint();
         }
       }
     });
+
   }
 
   private Point getLegalLocation(int x, int y){
@@ -114,10 +179,10 @@ class AreasPanel extends JPanel{
     return new Point(x, y);
   }
 
-  public void setSelector(int x, int y){
+  public Point setSelector(int x, int y){
     Point p = getLegalLocation(x, y);
     selector.setLocation(p.x, p.y);
-    repaint();
+    return p;
   }
 
   public void drawDashedLine(Graphics g, int x1, int y1, int x2, int y2){
@@ -149,11 +214,11 @@ class AreasPanel extends JPanel{
     else{
       g.drawImage(image, 15, 15, null);
     }
-    g.setXORMode(Color.GRAY);
+    g.drawImage(subimage, 380, 15, null);
+    g.setXORMode(Color.WHITE);
     Graphics2D g2d = (Graphics2D) g.create();
     Stroke dashed = new BasicStroke(1, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[]{3}, 0);
     g2d.setStroke(dashed);
-    g2d.draw(selector);
-
+    if(visible)g2d.draw(selector);
   }
 }
